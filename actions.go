9bb8207f18a0d3d912c15642eeb71d923d1282fc
ztrue <gztrue@gmail.com>
2017-08-13 15:02:32 +0300

    Refactor, fixes

--- actions.go
+++ actions.go
@@ -7,6 +7,7 @@ import (
 )
 
 var ErrNilParam = errors.New("nil param")
+var ErrEmptyEntity = errors.New("empty entity")
 
 // func ActionGetLocation(ctx *fasthttp.RequestCtx, id uint32) {
 //   l := GetLocation(id)
@@ -63,68 +64,9 @@ func ActionGetLocationAvg(ctx *fasthttp.RequestCtx, id uint32, v *fasthttp.Args)
 
 type DummyResponse struct {}
 
-func ActionUpdateLocation(ctx *fasthttp.RequestCtx, id uint32) {
-  body := ctx.PostBody()
-  if err := checkNils(ctx, body); err != nil {
-    ResponseStatus(ctx, 400)
-    return
-  }
-  e := new(Location)
-  if err := json.Unmarshal(body, e); err != nil {
-    ResponseStatus(ctx, 400)
-    return
-  }
-  if err := UpdateLocation(id, e); err != nil {
-    ResponseError(ctx, err)
-    return
-  }
-  ResponseJSON(ctx, DummyResponse{})
-}
-
-func ActionUpdateUser(ctx *fasthttp.RequestCtx, id uint32) {
-  body := ctx.PostBody()
-  if err := checkNils(ctx, body); err != nil {
-    ResponseStatus(ctx, 400)
-    return
-  }
-  e := new(User)
-  if err := json.Unmarshal(body, e); err != nil {
-    ResponseStatus(ctx, 400)
-    return
-  }
-  if err := UpdateUser(id, e); err != nil {
-    ResponseError(ctx, err)
-    return
-  }
-  ResponseJSON(ctx, DummyResponse{})
-}
-
-func ActionUpdateVisit(ctx *fasthttp.RequestCtx, id uint32) {
-  body := ctx.PostBody()
-  if err := checkNils(ctx, body); err != nil {
-    ResponseStatus(ctx, 400)
-    return
-  }
-  e := new(Visit)
-  if err := json.Unmarshal(body, e); err != nil {
-    ResponseStatus(ctx, 400)
-    return
-  }
-  if err := UpdateVisit(id, e); err != nil {
-    ResponseError(ctx, err)
-    return
-  }
-  ResponseJSON(ctx, DummyResponse{})
-}
-
 func ActionNewLocation(ctx *fasthttp.RequestCtx) {
-  body := ctx.PostBody()
-  if err := checkNils(ctx, body); err != nil {
-    ResponseStatus(ctx, 400)
-    return
-  }
-  e := new(Location)
-  if err := json.Unmarshal(body, e); err != nil {
+  var e *Location
+  if err := checkRequest(ctx, e); err != nil {
     ResponseStatus(ctx, 400)
     return
   }
@@ -136,13 +78,8 @@ func ActionNewLocation(ctx *fasthttp.RequestCtx) {
 }
 
 func ActionNewUser(ctx *fasthttp.RequestCtx) {
-  body := ctx.PostBody()
-  if err := checkNils(ctx, body); err != nil {
-    ResponseStatus(ctx, 400)
-    return
-  }
-  e := new(User)
-  if err := json.Unmarshal(body, e); err != nil {
+  var e *User
+  if err := checkRequest(ctx, e); err != nil {
     ResponseStatus(ctx, 400)
     return
   }
@@ -154,13 +91,8 @@ func ActionNewUser(ctx *fasthttp.RequestCtx) {
 }
 
 func ActionNewVisit(ctx *fasthttp.RequestCtx) {
-  body := ctx.PostBody()
-  if err := checkNils(ctx, body); err != nil {
-    ResponseStatus(ctx, 400)
-    return
-  }
-  e := new(Visit)
-  if err := json.Unmarshal(body, e); err != nil {
+  var e *Visit
+  if err := checkRequest(ctx, e); err != nil {
     ResponseStatus(ctx, 400)
     return
   }
@@ -169,6 +101,59 @@ func ActionNewVisit(ctx *fasthttp.RequestCtx) {
     return
   }
   ResponseJSON(ctx, DummyResponse{})
+}
+
+func ActionUpdateLocation(ctx *fasthttp.RequestCtx, id uint32) {
+  var e *Location
+  if err := checkRequest(ctx, e); err != nil {
+    ResponseStatus(ctx, 400)
+    return
+  }
+  if err := UpdateLocation(id, e); err != nil {
+    ResponseError(ctx, err)
+    return
+  }
+  ResponseJSON(ctx, DummyResponse{})
+}
+
+func ActionUpdateUser(ctx *fasthttp.RequestCtx, id uint32) {
+  var e *User
+  if err := checkRequest(ctx, e); err != nil {
+    ResponseStatus(ctx, 400)
+    return
+  }
+  if err := UpdateUser(id, e); err != nil {
+    ResponseError(ctx, err)
+    return
+  }
+  ResponseJSON(ctx, DummyResponse{})
+}
+
+func ActionUpdateVisit(ctx *fasthttp.RequestCtx, id uint32) {
+  var e *Visit
+  if err := checkRequest(ctx, e); err != nil {
+    ResponseStatus(ctx, 400)
+    return
+  }
+  if err := UpdateVisit(id, e); err != nil {
+    ResponseError(ctx, err)
+    return
+  }
+  ResponseJSON(ctx, DummyResponse{})
+}
+
+func checkRequest(ctx *fasthttp.RequestCtx, e interface{}) error {
+  body := ctx.PostBody()
+  if err := checkNils(ctx, body); err != nil {
+    return err
+  }
+  if err := json.Unmarshal(body, e); err != nil {
+    return err
+  }
+  if e == nil {
+    return ErrEmptyEntity
+  }
+  return nil
 }
 
 func checkNils(ctx *fasthttp.RequestCtx, body []byte) error {
