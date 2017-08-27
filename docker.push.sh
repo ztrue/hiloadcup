1e6de0c938264e2e93e6d686bab06afd7ff6b409
ztrue <gztrue@gmail.com>
2017-08-28 00:43:47 +0300

    Docker commands

---
+++ docker.push.sh
@@ -0,0 +1,1 @@
+docker tag hlc stor.highloadcup.ru/travels/kiwi_hunter && docker push stor.highloadcup.ru/travels/kiwi_hunter
---
+++ docker.run.sh
@@ -0,0 +1,1 @@
+docker stop hi; docker build -t hlc . && docker run --name hi --rm -p 8001:80 -t hlc
